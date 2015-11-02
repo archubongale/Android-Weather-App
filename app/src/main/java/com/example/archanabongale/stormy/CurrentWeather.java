b5cb5b22241e7384b39256df54ee53c46c8bc0a3
archubongale <archubongale@gmail.com>
2015-11-02 12:27:50 -0800

    Completed app

---
+++ app/src/main/java/com/example/archanabongale/stormy/CurrentWeather.java
@@ -0,0 +1,122 @@
+package com.example.archanabongale.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by archanabongale on 11/1/15.
+ */
+public class CurrentWeather {
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String mIcon) {
+        this.mIcon = mIcon;
+    }
+
+    public int getIconId() {
+        int iconId = R.mipmap.clear_day;
+
+        if(mIcon.equals("clear-day")) {
+            iconId = R.mipmap.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.mipmap.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.mipmap.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.mipmap.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.mipmap.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.mipmap.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.mipmap.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.mipmap.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.mipmap.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.mipmap.cloudy_night;
+        }
+
+        return iconId;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime() * 1000);
+        String timeString = formatter.format(dateTime);
+
+        return timeString;
+    }
+
+    public void setTime(long mTime) {
+        this.mTime = mTime;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double mTemperature) {
+        this.mTemperature = mTemperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double mHumiduty) {
+        this.mHumidity = mHumiduty;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double mPrecipChance) {
+        this.mPrecipChance = mPrecipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String mSummary) {
+        this.mSummary = mSummary;
+    }
+
+
+}
